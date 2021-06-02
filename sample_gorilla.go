951d29555824a83fc3f87ac8e53bcd61b970191b
mofumofuchan <you_oki1@yahoo.co.jp>
2021-06-02 23:58:47 +0900

    Add codes

--- sample_gorilla.go
+++ sample_gorilla.go
@@ -3,7 +3,14 @@ package main
 import (
     "fmt"
     "net/http"
+
     "github.com/gorilla/mux"
+    "github.com/gorilla/sessions"
+)
+
+var (
+    key = []byte("super-secret-key")
+    store = sessions.NewCookieStore(key)
 )
 
 func main() {
@@ -14,6 +21,8 @@ func startWebServer() error {
     r := mux.NewRouter().StrictSlash(true)
 
     r.HandleFunc("/", rootPage)
+    r.HandleFunc("/login", login)
+    r.HandleFunc("/secret", secret)
 
     return http.ListenAndServe(fmt.Sprintf(":%d", 3000), r)
 }
@@ -21,5 +30,27 @@ func startWebServer() error {
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "welcome to my go api server!\n")
+
     fmt.Println("Root endpoint is hooked!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+    session, _ := store.Get(r, "cookie-name")
+
+    fmt.Fprintf(w, "session authenticated!\n")
+    fmt.Println("session authenticated!")
+
+    session.Values["authenticated"] = true
+    session.Save(r, w)
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+    session, _ := store.Get(r, "cookie-name")
+
+    if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+        http.Error(w, "Forbidden", http.StatusForbidden)
+        return
+    }
+
+    fmt.Fprintln(w, "the cake is a lie!")
 }
