8122be9b46fc4638438ea94872fcbcd21d1c7c38
mofumofuchan <you_oki1@yahoo.co.jp>
2021-06-05 22:48:11 +0900

    cookie操作のサンプルコードを追加

--- sample_net_http.go
+++ sample_net_http.go
@@ -3,10 +3,12 @@ package main
 import (
     "net/http"
     "fmt"
+    "log"
 )
 
 func main() {
     http.HandleFunc("/", rootPageHandler)
+    http.HandleFunc("/login", login)
 
 
     http.ListenAndServe(":3000", nil)
@@ -14,9 +16,26 @@ func main() {
 
 func rootPageHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "welcome to my go api server\n")
-
     fmt.Println("Root endpoint is hooked!")
+
+    cookie, err := r.Cookie("hoge")
+
+    if err != nil {
+        log.Fatal("Cookie: ", err)
+    }
+
+    v := cookie.Value
+    fmt.Println(v)
+    fmt.Fprintf(w, "read cookie\n")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
+    cookie := &http.Cookie{
+        Name: "hoge",
+        Value: "bar",
+    }
+    http.SetCookie(w, cookie)
 
+    fmt.Fprintf(w, "set cookie\n")
+    fmt.Println("set cookie!")
+}
